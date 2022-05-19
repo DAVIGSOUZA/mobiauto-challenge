df220d9a15a72d38d09b019fe33b9c8694ea218c
Davi Garcia <davigs@hotmail.com>
2022-05-19 16:25:29 -0300

    add exercise 3 answer

--- src/Exercises/Exercise3/index.js
+++ src/Exercises/Exercise3/index.js
@@ -3,7 +3,6 @@
 // API aberta não precisa de token
 // Documentação
 // https://rickandmortyapi.com/documentation/#rest
-
 // Ex de Saida: [
 //   {
 //     nome: 'Rick Sanchez',
@@ -37,8 +36,42 @@
 //   }
 // ]
 
+const { default: axios } = require("axios")
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character/'
+
+const parseHumanCharacter = (character) => {
+  return {
+    nome: character.name,
+    genero: character.gender === "Male" ? "Homem" : "Mulher",
+    avatar: character.image,
+    especie: 'Humano'
+  }
+}
+
+// simple aproach
+// async function getRicAndMortyCharacters() {
+//   const charactersId = '1,2,3,4,5'
+//   return await axios.get(BASE_URL+charactersId)
+//     .then(res => res.data.map(character => parseHumanCharacter(character)))
+// }
+
+// alternative aproach
+// assuming we dont have characters Ids
+// assuming any response for the requested name will suffice, as Rick and Morty has multiple realities
 async function getRicAndMortyCharacters() {
-  //you code here...
+  const characters = [
+    'rick sanchez',
+    'morty smith',
+    'summer smith',
+    'beth smith',
+    'jerry smith'
+  ]
+  return await axios.all(characters.map(character => (
+    axios.get(`${BASE_URL}?name=${character}&status=alive`)
+  ))).then(responses => responses.map(response => (
+    parseHumanCharacter(response.data.results[0])
+  )))
 }
 
 module.exports = getRicAndMortyCharacters;
